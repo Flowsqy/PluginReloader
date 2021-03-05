a93a144539b17e8d355a89c4de9b42f9064fd7a2
Flowsqy <Flo504.Dev@gmail.com>
2021-03-06 00:14:17 +0100

    Add plugin base

---
+++ src/main/java/fr/flowsqy/pluginreloader/PluginReloaderPlugin.java
@@ -0,0 +1,13 @@
+package fr.flowsqy.pluginreloader;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PluginReloaderPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+
+    }
+}
